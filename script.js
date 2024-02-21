5bf0da1bfd36ad5a763756d5c5c9c654094b26c1
BryEmmanuel <bryanlim1129@gmail.com>
2024-02-21 22:28:15 +0800

    Snake progress update
    movement done , but still have some bugs.

--- script.js
+++ script.js
@@ -2,19 +2,21 @@
 
 const gameBoard = document.getElementById("game-board");
 // defining variables
+let cell = [{ x: 0, y: 0 }];
+// cell would represent each individual square on the grid
 let snake = [{ x: 10, y: 10 }];
-let cell = [{ x: 15, y: 15 }];
+// snake is an array of kvp for coordinates
+let food = changeFood();
+// food will be generated randomly, so we have to use a function to randomly generate possible x and y values within the grid
+// note that food should not be generated on the snake itself - remember to check for condition
 
 function createBoard() {
   // clear the board everytime this function runs
   gameBoard.innerHTML = "";
   createSnake();
-  cell.forEach(function (cell) {
-    const elementFood = createFood("div", "food");
-    setLocation(elementFood, cell);
-    gameBoard.appendChild(elementFood);
-  });
+  createFood();
 }
+
 // snake array contains object that has x and y coordinates which corresponds to the location on the grid
 
 function createSnake() {
@@ -22,20 +24,32 @@ function createSnake() {
   // this function creates a 'div class' of 10px x 10px to represent 1 grid cell of each snake section, using the css .snake
   // define snake outside this function
   // loop through snake's kvp to perform a function for each kvp
+  // console.log(snake);
   snake.forEach(function (cell) {
-    const elementSnake = createFood("div", "snake");
+    const elementSnake = createCell("div", "snake");
     setLocation(elementSnake, cell);
     gameBoard.appendChild(elementSnake);
   });
 }
 
+function createFood() {
+  // food should be like snake, an array of kvp, except that it only has 1 kvp.
+  // define food above
+
+  food.forEach(function (cell) {
+    const elementFood = createCell("div", "food");
+    setLocation(elementFood, cell);
+    gameBoard.appendChild(elementFood);
+  });
+}
+
 // creating individual cell/box - to be used for snake or food
-function createFood(tag, className) {
+function createCell(tag, className) {
   // create the cell/box using 'tag' and 'className' as parameters
   const box = document.createElement(tag);
   // set the cell's/box's class name to the parameter I define : which can be snake or food
   box.className = className;
-  // box = div.food when createFood();
+  // box = div.food when console.log(createFood());
   return box;
 }
 
@@ -47,41 +61,76 @@ function setLocation(cell, position) {
   cell.style.gridRow = position.y;
 }
 
-// movement of Snake
-function moveSnake() {
-  document.body.addEventListener("keydown", function (event) {
-    const key = event.key;
-    switch (key) {
-      case "ArrowLeft":
-        console.log("left arrow pressed");
-        console.log(snake[0].x);
-        for (let i = 0; i < snake.length; i++) {
-          return (snake = snake[i].x - 1);
-        }
-        break;
-      case "ArrowRight":
-        console.log("right arrow pressed");
-        for (let i = 0; i < snake.length; i++) {
-          snake[i].x + 1;
-        }
-        break;
-      case "ArrowUp":
-        console.log("up arrow pressed");
-        for (let i = 0; i < snake.length; i++) {
-          snake[i].y - 1;
-        }
-        break;
-      case "ArrowDown":
-        console.log("down arrow pressed");
-        for (let i = 0; i < snake.length; i++) {
-          snake[i].y + 1;
-        }
-        break;
-    }
-  });
+function changeFood() {
+  const x = Math.floor(Math.random() * 30);
+  const y = Math.floor(Math.random() * 30);
+  return [{ x, y }];
 }
-moveSnake();
-/*
+createBoard();
+
+// movement of Snake
+let direction = "right";
+function moveSnake() {
+  let head = { x: snake[0].x, y: snake[0].y };
+  console.log(head);
+  console.log(food);
+  switch (direction) {
+    case "right":
+      head.x++;
+      break;
+    case "down":
+      head.y++;
+      break;
+    case "left":
+      head.x--;
+      break;
+    case "up":
+      head.y--;
+      break;
+  }
+
+  // unshift to add to the first element
+
+  snake.unshift(head);
+
+  //console.log(head);
+  // when snake eats food
+  if (head.x === food[0].x && head.y === food[0].y) {
+    food = changeFood();
+  } else {
+    // pop to remove the last element
+    snake.pop();
+  }
+}
+
+// event listener for keypress
+
+// refer to https://www.toptal.com/developers/keycode for keycode
+document.addEventListener("keydown", (event) => {
+  switch (event.key) {
+    case "ArrowUp":
+      direction = "up";
+      break;
+    case "ArrowRight":
+      direction = "right";
+      break;
+    case "ArrowDown":
+      direction = "down";
+      break;
+    case "ArrowLeft":
+      direction = "left";
+      break;
+  }
+});
+
+// setInterval runs the functions every 'delay' - in this case, 1000ms.
+let gameInterval = setInterval(function () {
+  moveSnake();
+  createBoard();
+
+  //to stop timer
+  clearInterval(gameInterval);
+}, 400);
 
 // there should be a gameloop function that continuously runs the game
 
@@ -172,3 +221,7 @@ moveSnake();
 // future plan - think of PvP , accept 'w a s d' for another player
 // difficulty or additional resource like food but gives a penalty instead
 // speed
+
+// 2 difficulties that was brought up during the project proposals
+// collision (with itself)
+// how does the snake know how to follow the direction when turning corners
